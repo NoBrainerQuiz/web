50c17e69b4597f166014696536331e10dd8a2d3c
Glenn Hamilton-Smith <UP824726@uni.ds.port.ac.uk>
2018-01-31 12:27:23 +0000

    Added Question Page and CSS/JS
    Added the files for the question view and its associated Javascript and
    CSS files

---
+++ public/js/question.js
@@ -0,0 +1,15 @@
+function centerAnswers() {
+  var yPadding = 0;
+  var ansBoxHeight = parseInt($(".ans-box").css("height"));
+  var containerTextHeight = parseInt( $(".center-container").css("height"));
+  var yPadding = (ansBoxHeight - containerTextHeight) / 2;
+  $(".center-container").css("top", yPadding);
+}
+
+$(window).resize( function() {
+  centerAnswers();
+})
+
+$(document).ready( function() {
+  centerAnswers();
+})
