cee5ec013ea51d9ec6abf833fd802a9fabe49d6d
Matt Kent <mattokent@icloud.com>
2018-02-21 16:04:15 +0000

    Login form now complete bootstrap

--- resources/views/quiz_host/auth/layout/auth.blade.php
+++ resources/views/quiz_host/auth/layout/auth.blade.php
@@ -3,6 +3,8 @@
 <head>
   <meta charset="utf-8">
   <meta name="csrf-token" content="{{ csrf_token() }}">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
   <link rel="stylesheet" type="text/css" href="{{ asset('css/theme.css') }}">
   <link rel="stylesheet" type="text/css" href="{{ asset('css/login.css') }}">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
@@ -13,11 +15,21 @@
 </head>
 
 <body>
-    <div id="top-container">
-        <img src="{{ asset('img/noBrainer.png') }}" id="noBrainer" alt="logo" onclick="location.href='about.html'"/>
+    <div class="container" id="top-container">
+        <img style="max-width:100%; max-height:100%; margin: 0 auto; display: block;" src="{{ asset('img/noBrainer.png') }}" id="noBrainer" alt="logo" onclick="location.href='about.html'"/>
     </div>
-    <div id="container">
+    <div class="jumbotron jumbotron-fluid text-center" id="main-page-container">
+      <div class="container container-fluid" id=id="main-page-container">
         @yield('container_content')
     </div>
+    <div class="contaier container-fluid text-center">
+        <p id="version-number">Version 0.0.1-alpha-1</p>
+      </div>
+    </div>
+<!--External Scripts-->
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <!--End External Scripts-->
 </body>
 </html>
--- resources/views/quiz_host/auth/login.blade.php
+++ resources/views/quiz_host/auth/login.blade.php
@@ -4,17 +4,29 @@
 
 @section('container_content')
 
+<h4 style="margin: 0 auto;">Sign in to your NoBrainer account...</h4><hr />
 @if ($errors->all())
-  <div class="loginError">
+<div class="alert alert-danger" role="alert" style="width: 50%; margin: 0 auto; text-align: left;">
+  <strong>Ooops, please correct the below errors before logging in:</strong><br />
     @foreach ($errors->all() as $error)
       {{ $error }} <br />
     @endforeach
   </div>
+  <hr />
 @endif
-  <form id="login" action="{{ route('login') }}" method="post">
+  <form action="{{ route('login') }}" method="post" style="width: 50%; margin: 0 auto;">
     {{ csrf_field() }}
-    <input type="text" name="email" placeholder="Email" value="{{ old('email') }}"><br>
-    <input type="password" name="password" placeholder="Password..."><br>
-    <input id="signUp" type="submit" name="submit" value="Login">
+    <div class="form-group">
+    <label style="float: left;">Username/Email:&nbsp;&nbsp;<a href="#" class="badge badge-danger">?</a></label><br />
+      <input id="email-entry" class="form-control" type="text" name="email" placeholder="Enter your username/email..." value="{{ old('email') }}">
+</div>
+    <div class="form-group">
+      <label style="float: left;">Password:&nbsp;&nbsp;<a href="#" class="badge badge-danger">?</a></label><small><span style="float: right; margin-top: 8px;"><a href="{{ route('password.request') }}">Forgot password?</a></span></small><br />
+      <input id="password-entry" class="form-control" type="password" name="password" placeholder="Enter your password...">
+    </div>
+    <div class="form-group">
+    <a href="{{ route('register') }}" style="float: left;">Don't have an account?</a>
+    <button style="float: right; cursor: pointer;" type="submit" class="btn btn-warning">Sign me in &raquo;</button>
+</div>
   </form>
 @endsection
